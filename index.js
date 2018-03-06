0c147157f779d2c81005c276988519211a23b417
Elvis Estevan <elvis.estevan65@gmail.com>
2018-03-06 20:51:15 -0300

    Adicionado tratativa no ltksistemas.com.br

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 
 app.use(function (req, res, next) {
+  if (req.headers.host === "ltksistemas.com.br") {
+    res.redirect("https://erp." + req.headers.host + req.url);
+  }
   if (!/https/.test(req.protocol)) {
     res.redirect("https://" + req.headers.host + req.url);
   } else {
